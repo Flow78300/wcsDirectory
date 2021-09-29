e12c3154d23e2c8fd262b3cae58272d5e5168402
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-09-29 15:46:32 +0200

    modif file index.php

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WCS Directory Home page</title>
+</head>
+<body>
+    <?php include '_header.php' ?>
+    <section class="WCS-img"></section>
 
+    <section class="presentation-text">
+        <h1>Meet the Team PHP Blue-Mûmakil</h1>
+        <p>Ils étaient 66 dans cette quête du Graal, ils ne sont plus que 14, 
+            guidés par Jean-François, à la fin ils finiront tous Dev!
+        </p>
+    </section>
+    <section class="links-trombi">
+        <a href="#">Cliquer ici pour accéder au trombinoscope</a>
+    </section>
+
+    <?php include '_footer.php' ?>
+</body>
+</html>
