691f6646eed88e99a9f706d2826ce726dea264e3
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-09-29 17:38:29 +0200

    modif fichier index.php et ajout de assets/style.css

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WCS Directory Home page</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
     <?php include '_header.php' ?>
-    <section class="WCS-img"></section>
+    <main>
+        <section class="WCS-img"></section>
 
-    <section class="presentation-text">
-        <h1>Meet the Team PHP Blue-Mûmakil</h1>
-        <p>Ils étaient 66 dans cette quête du Graal, ils ne sont plus que 14, 
-            guidés par Jean-François, à la fin ils finiront tous Dev!
-        </p>
-    </section>
-    <section class="links-trombi">
-        <a href="#">Cliquer ici pour accéder au trombinoscope</a>
-    </section>
+        <section class="presentation-text">
+            <h1>Meet the Team PHP Blue-Mûmakil</h1>
+            <p>Ils étaient 66 dans cette quête du Graal, ils ne sont plus que 14,
+                guidés par Jean-François, à la fin ils finiront tous Dev!
+            </p>
+
+            <p>L'annuaire en ligne est un outil de mise en relation entre élèves, futurs diplômés, avec divers acteurs de l'univers de la Tech.</br>
+                </br>
+                Chacun d'entre vous est invité à mettre à jour sa fiche personnelle régulièrement. </br>
+            </p>
+
+        </section>
+        <section class="links-trombi">
+            <a href="#">Cliquer ici pour accéder au trombinoscope</a>
+        </section>
+    </main>
 
     <?php include '_footer.php' ?>
 </body>
+
 </html>
