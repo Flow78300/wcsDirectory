fb25b87bd0e4020585e31c6c8a8efa82551d261b
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-10-01 18:48:57 +0200

    modif index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WCS Directory Home page</title>
-    <link rel="stylesheet" href="assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
@@ -15,7 +15,7 @@
          <section class="WCS-img"></section>
 
          <section class="presentation-text">
-             <h1>Meet the Team PHP Blue-Mûmakil</h1>
+             <h1>Meet the Team</h1>
             <p>Ils étaient 66 dans cette quête du Graal, ils ne sont plus que 14,
                 guidés par Jean-François, à la fin ils finiront tous Dev!
             </p>
