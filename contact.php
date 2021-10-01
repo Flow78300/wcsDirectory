8346e8293a46b19fd39450c5dc9efcccd70f29cd
BinGitMaker <dbinoisdevpro@gmail.com>
2021-10-01 10:32:13 +0200

    footer logoMumakil CssFine Contact fiche_perso Trombi

--- contact.php
+++ contact.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="assets/styleFine.css">
+    </head>
 
+<body>
+    <?php include '_header.php'?>
+    
+<form>
+      <h2>Contact</h2>
+      <div class="formulaire">
+        <input type="Nom Prenom" placeholder="Frodon Sacquet" />
+        <input type="@mail" placeholder="frodon_saquet@hacker.com" />
+        <input type="Objet" placeholder="Super Quête" />
+        <textarea placeholder="Avez vous retrouvé mon Precieux?"></textarea>
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+
+
+<?php include '_footer.php' ?>
+</body>
+
+</html>
