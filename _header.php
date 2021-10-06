e4273db08d8f0b59275c2b2937052002a1141ac1
Florian Dugué <florian.dugue@gmail.com>
2021-10-06 11:04:34 +0200

    update header php/css

--- _header.php
+++ _header.php
@@ -2,44 +2,30 @@
 <header>
 
 <nav class="navbar">
-        <div class="container">
+        <div class="bloc-container">
             <div class="navbar-brand">
-                <a class="navbar-item" href="#">
+                <a class="navbar-item" href="index.php">
                 <img src= "assets/wcs_resize.png" alt="Logo">
                 </a>
-      <h1> Trombi Blue Mumakil </h1>
+           <h2> Meet the Team PHP Blue-Mûmakil </h2>
 
-
-
-      <ul> 
-        <li><a href="index.php"> Connectez vous </a></li>
-        <li><a href="trombi.php"> Trombinoscope </a></li>
-        <li><a href="search.php"> Recherche Wilder par régions </a></li>
-        <li><a href="recrut.php"> Espace recrutement </a></li>
-      </ul>
-    </nav>
-
-
-           <!-- </div>
-            <div id="navbarMenu" class="navbar-menu">
-                <div class="navbar-end">
-                    <a href="#Sign-in" class="navbar-item">
-                        Connectez-vous
-
-                    <a href="#trombi" class="navbar-item">
-                        Trombinoscope
-                    </a>
-                    <a href="#recherche" class="navbar-item">
-                        Recherche Wilder par régions
-                    </a>
-                    <a href="#recruteur" class="navbar-item">
-                        Espace recrutement
-                    </a>
-
-                    </a> -->
-                </div>
-            </div>
+            <nav role="navigation">
+              <div id="menuToggle">
+                <input type="checkbox" />
+                  <span></span>
+                  <span></span>
+                  <span></span>
+               <ul id="menu">
+                 <li><a href="index.php">Home</a></li>
+                 <li><a href="trombi.php">Trombi</a></li>
+                 <li><a href="search.php">Wilders par région</a></li>
+                 <li><a href="recrut.php">Recruteurs</a></li>
+               </ul>
+             </div>
+           </nav>                  
+         </div>
         </div>
+      </div>
     </nav>
 </header>
 
