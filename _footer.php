cf42a949a0ba23a4ee7782c7868bc48d35547803
Romain Doukhan <romaindoukhan@gmail.com>
2021-10-06 11:33:16 +0200

    add footer css and php

--- _footer.php
+++ _footer.php
@@ -1,8 +1,31 @@
-
-<footer>
-    <ul>
-        <li><img src= "assets/wcs_resize.png" alt="Logo" classe = "wildImg"/></li>
-        <li><a class="wcs_link_shadow" href="https://www.wildcodeschool.com">Into the Wild Code School</a></li>
-        <li><a>Parce que le developpement nous unis.</a></li>
-    </ul>
-</footer>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="footer-basic">
+        <footer>
+            <div class="social">
+                <a href="https://www.facebook.com/WildCodeSchool/"><i class="icon ion-social-facebook"></i></a>
+                <a href="https://twitter.com/wildcodeschool"><i class="icon ion-social-twitter"></i></a>
+                <a href="https://www.linkedin.com/school/wild-code-school"><i class="icon ion-social-linkedin"></i></a>
+                <a href="https://www.instagram.com/wildcodeschoolofficial/?hl=en"><i class="icon ion-social-instagram"></i></a>
+                <a href="https://github.com/WildCodeSchool"><i class="icon ion-social-github"></i></a>
+                <a href="https://www.youtube.com/channel/UCi99G_0QPx5sYsK8zdvQzfw"><i class="icon ion-social-youtube"></i></a>
+            </div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="index.php">Acceuil</a></li>
+                <li class="list-inline-item"><a href="trombi.php">Trombinoscope</a></li>
+                <li class="list-inline-item"><a href="recrut.php">Recrutement</a></li>
+                <li class="list-inline-item"><a href="search.php">Recherche Wilders</a></li>
+            </ul>
+            <p class="copyright">Wild Code School © 2013-2021</p>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
