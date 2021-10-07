b1abbff566bce867dba0f0c2a4d3937c53b0dd60
Florian Dugué <florian.dugue@gmail.com>
2021-10-07 12:19:01 +0200

    update header

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WCS Directory Home page</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 </head>
 
 <body>
-    <?php include '_loader.php' ?>
+    <?php 
+    // include '_loader.php' 
+    ?>
     <?php include '_header.php' ?>
      <main>
          <section class="WCS-img"></section>
