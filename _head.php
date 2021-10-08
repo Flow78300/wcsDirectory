3c74a820d92e3962b566d237271a4bf0412a2799
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-10-08 11:32:03 +0200

    création fichier _head et MAJ index.php

---
+++ _head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WCS Directory Home page</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
--- fiche_perso.php
+++ fiche_perso.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fiche personnelle de Prénom NOM</title>
-    <link rel="stylesheet" href="assets/css/fichier_perso.css">
-</head>
+<?php include '_head.php' ?>
 
 <body>
 <?php include '_header.php'?>
