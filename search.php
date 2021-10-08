55c948bb6527b012fea35402cf0dafdfc92aeb9b
Florian Dugué <florian.dugue@gmail.com>
2021-10-08 12:34:47 +0200

    add search

--- search.php
+++ search.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/CSS/style.css">
-    <title>search</title>
+    <title>Search</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 </head>
 <body>
+    <?php include '_loader.php' ?>
+    <?php include '_header.php' ?>
+
     <h1>Erreur 404 Hello wilders ;-)</h1>
+
+    <?php include '_footer.php' ?>
+    
+    <script src="script.js"></script>
 </body>
 </html>
